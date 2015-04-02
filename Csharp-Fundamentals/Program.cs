a3f2d21d14ba9d1e0eacd8bf2a05f88dd6e9ebd8
Stevo5o <stevo.joc@gmail.com>
2015-04-02 16:42:28 +0100

    VS 13 keyboard shortcuts

--- Csharp-Fundamentals/Program.cs
+++ Csharp-Fundamentals/Program.cs
@@ -11,6 +11,10 @@ namespace Csharp_Fundamentals
         static void Main(string[] args)
         {
             Console.Write("Hello World!");
+            //keyboard shortcuts
+            //    ctrl k, ctrl c comment 
+            //    ctrl k, ctrl u uncomment 
+            //    duplicte line end of line ctrl c crtl v
         }
     }
 }

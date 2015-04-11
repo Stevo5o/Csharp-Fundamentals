9710765a1487e1b210d65eaea57443721cf4c666
Stevo5o <stevo.joc@gmail.com>
2015-04-11 09:02:45 +0100

    Region and conditionals

--- Csharp-Fundamentals/Program.cs
+++ Csharp-Fundamentals/Program.cs
@@ -11,12 +11,22 @@ namespace Csharp_Fundamentals
     {
         static void Main(string[] args)
         {
-            Person stej = new Person("SteJ", "O'Connor"),
-                kate = new Person("Kate", "Bush");
+            #region Old Class-related code
+            //Person stej = new Person("SteJ", "O'Connor"),
+            //    kate = new Person("Kate", "Bush");
 
-            string greeting = stej.SayHello(kate);
+            //string greeting = stej.SayHello(kate);
 
-            Console.WriteLine(greeting);
+            //Console.WriteLine(greeting); 
+            #endregion
+            bool result = (6 <= 6);
+
+            bool resultString = "test" == "Test";
+
+            Console.WriteLine(result);
+
+            Console.WriteLine(resultString);
         }
     }
 }
+

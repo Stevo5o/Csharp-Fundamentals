3654318aa1155e1d4c3fe9c334252a8c564e08a3
Stevo5o <stevo.joc@gmail.com>
2015-04-13 17:28:23 +0100

    Abstract Classes

---
+++ MyFirstDataTypes/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Csharp_Fundamentals.MyFirstDataTypes
+{
+    class Square : Shape
+    {
+        public double Length { get; private set; }
+        public Square(double sideLength)
+        {
+            Length = sideLength;
+
+        }
+        public override double Area
+        {
+            get { return Length * Length; }
+        }
+    }
+}

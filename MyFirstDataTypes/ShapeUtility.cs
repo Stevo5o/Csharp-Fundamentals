6aa71eaf05cfa0a3595389a93cec9d59909ba586
Stevo5o <stevo.joc@gmail.com>
2015-04-15 11:51:16 +0100

    Extension Methods

--- MyFirstDataTypes/ShapeUtility.cs
+++ MyFirstDataTypes/ShapeUtility.cs
@@ -7,7 +7,7 @@ namespace Csharp_Fundamentals.MyFirstDataTypes
 {
     public static class ShapeUtility
     {
-        public static bool IsPolygon(Shape shape)
+        public static bool IsPolygon(this Shape shape)
         {
             return shape.Sides >= 3;
         }

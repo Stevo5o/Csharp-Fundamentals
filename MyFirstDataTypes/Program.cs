302715aeb7b452d2cb867feae65cdc03fa85675b
Stevo5o <stevo.joc@gmail.com>
2015-04-17 09:49:00 +0100

    Using a delegate instad of method

---
+++ MyFirstDataTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Fundamentals.MyFirstDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> {
+                "Justy",
+                "Jimmy",
+                "Jonny",
+                "Jenny",
+                "Jakey",
+                "Julie",
+                "Jeffy",
+                "Jammy",
+                "Jacky"
+            };
+
+            //var nameWithE = names.FindAll(FindNamesWithE);
+            var nameWithE = names.FindAll(
+                delegate(string name)
+                {
+                    return name.IndexOf("e") == 1;
+                }
+            );
+
+            foreach (var name in nameWithE)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        //static bool FindNamesWithE(string name)
+        //{
+        //    return name.IndexOf("e") == 1;
+        //}
+    }
+}
+
+
+
+
+
